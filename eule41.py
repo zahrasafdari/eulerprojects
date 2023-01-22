7de3d57b7fdbdc6004e7be138155aeae8336281e
Zahra <79918668+zahrasafdari@users.noreply.github.com>
2023-01-22 14:25:08 +0330

    First

--- eule41.py
+++ eule41.py
@@ -23,3 +23,6 @@ for z in zarayeb3va5:
     total_list.append(z)
 
 print(sum(total_list))
+
+
+vmess://eyJhZGQiOiI5Mi4xMTkuNTYuMjQiLCJhaWQiOiIwIiwiaG9zdCI6ImFtdC5jb20iLCJpZCI6IjA1MmVmOTVlLTMzNzktNDU1Mi1mYmU1LTRiYjU5NTg2YzQ3NyIsIm5ldCI6InRjcCIsInBhdGgiOiIvIiwicG9ydCI6IjI1NTMyIiwicHMiOiJ4b3hvLWlyYW5jZWxsIiwic2N5IjoiYXV0byIsInNuaSI6IiIsInRscyI6Im5vbmUiLCJ0eXBlIjoiaHR0cCIsInYiOiIyIn0=
